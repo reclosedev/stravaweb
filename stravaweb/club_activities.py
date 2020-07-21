58ebfd043b8b814bcbfc18db4dc7a6d2f61bb38b
Roman Haritonov <r.haritonov@osahp.com>
2020-07-21 19:24:23 +0300

    Add totals output

--- stravaweb/club_activities.py
+++ stravaweb/club_activities.py
@@ -26,13 +26,21 @@ def main():
     if not cl.is_logged_in():
         print('Login to strava using {}'.format(settings.CREDENTIALS[0][0]))
         cl.login()
+
+    total_new = 0
     for club_id in settings.CLUBS:
-        load_club_activities(cl, db, club_id)
-    print('Elapsed {:.2f} seconds. Activities in DB {}'.format(time.time() - started_at, db['activities'].count()))
+        new = load_club_activities(cl, db, club_id)
+        total_new += new
+    print(
+        'Elapsed {:.2f} seconds. New activities: {}. Total activities in DB: {}'.format(
+            time.time() - started_at, total_new, db['activities'].count()
+        )
+    )
 
 
 def load_club_activities(client, db, club_id):
     current_count = db['activities'].count(club_id=club_id)
+    new_activities_saved = 0
     print('Downloading club {} activities ({} in db)'.format(club_id, current_count))
     for i, act in enumerate(client.get_club_activities(club_id)):
         act_string = '[{datetime} {athlete_name} ({athlete_id}) - {title}  ({activity_id})]'.format(**act)
@@ -64,6 +72,10 @@ def load_club_activities(client, db, club_id):
         act['club_id'] = club_id
         types = {'latlng': JSON, 'time': JSON, 'velocity_smooth': JSON}
         db['activities'].insert_ignore(act, ['activity_id'], types=types)
+        new_activities_saved += 1
+
+    print('New activities for club {}: {}'.format(club_id, new_activities_saved))
+    return new_activities_saved
 
 
 if __name__ == '__main__':
